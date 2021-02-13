8a4e6c9fc90aac711ee26d3811eaa7a27e43edb4
cajunkee <75953821+cajunkee@users.noreply.github.com>
2021-02-13 12:50:29 -0600

    First commit

---
+++ src/com/homework/TypesVariablesOperators.java
@@ -0,0 +1,18 @@
+package com.homework;
+
+public class TypesVariablesOperators {
+
+	public static void main(String[] args) {
+		System.out.println("I feel like Java is my favorite thus far.");
+		int k = 44;
+		int r = 25;
+		int l = 43;
+		System.out.println(k + r - l);
+		System.out.print("There's so much to learn, but I'm doing this!!\n");
+		System.out.println(k -r - l);
+		String favoriteDrink = "Waterloo coconut";
+		System.out.println(favoriteDrink + " is the best thing ever. It's like beer to me!!");
+		
+	}
+
+}
